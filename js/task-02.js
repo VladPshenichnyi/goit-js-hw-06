6b6a0a9b5db1e97e2c6f0f6131b2df90e00d5179
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2022-01-14 19:11:19 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,5 @@
+const setEl = document.querySelector('#ingredients');
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -5,4 +7,11 @@ const ingredients = [
   'Tomatos',
   'Herbs',
   'Condiments',
+  
 ];
+for (let i = 0; i < ingredients.length; i += 1) {
+  const setItemEl = document.createElement("li");
+  setItemEl.textContent = ingredients[i]
+  setItemEl.className = 'item'
+  setEl.append(setItemEl)
+};
