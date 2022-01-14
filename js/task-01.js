798263fadce678aed67be718b52653dbd8b78adf
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2022-01-14 18:20:03 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,19 @@
+// Решение 1
 const allCategoriesItemSetEl = document.querySelector('#categories')
-
 console.log('Number of categories :>> ', allCategoriesItemSetEl.children.length);
-console.log('Category :>> ', allCategoriesItemSetEl.children[0].firstElementChild.textContent);
-console.log('Element :>> ', allCategoriesItemSetEl.children[0].children[1].children.length);
-console.log('Category :>> ', allCategoriesItemSetEl.children[1].firstElementChild.textContent);
-console.log('Element :>> ', allCategoriesItemSetEl.children[1].children[1].children.length);
-console.log('Category :>> ', allCategoriesItemSetEl.children[2].firstElementChild.textContent);
-console.log('Element :>> ', allCategoriesItemSetEl.children[2].children[1].children.length)
+const allEl = allCategoriesItemSetEl.querySelectorAll('.item');
+    for (let i = 0; i < allEl.length; i += 1) {
+        console.log('Category :>> ', allCategoriesItemSetEl.children[i].firstElementChild.textContent);
+        console.log('Element :>> ', allCategoriesItemSetEl.children[i].children[1].children.length);
+    };
+
+
+// Решение 2
+// const allCategoriesItemSetEl = document.querySelector('#categories')
+// console.log('Number of categories :>> ', allCategoriesItemSetEl.children.length);
+// console.log('Category :>> ', allCategoriesItemSetEl.children[0].firstElementChild.textContent);
+// console.log('Element :>> ', allCategoriesItemSetEl.children[0].children[1].children.length);
+// console.log('Category :>> ', allCategoriesItemSetEl.children[1].firstElementChild.textContent);
+// console.log('Element :>> ', allCategoriesItemSetEl.children[1].children[1].children.length);
+// console.log('Category :>> ', allCategoriesItemSetEl.children[2].firstElementChild.textContent);
+// console.log('Element :>> ', allCategoriesItemSetEl.children[2].children[1].children.length)
